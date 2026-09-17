26d338e2b42c4df13d707d265b25f53539d02774
NikoBK <112914618+NikoBK@users.noreply.github.com>
2026-09-17 11:34:21 +0200

    create build configuration

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * file: main.cpp
+ * date: sep 17 2026
+ * author: nikobk
+ */
+
+#include "opencv2/core/version.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "OpenCV version: " << CV_VERSION << std::endl;
+    return 0;
+}
